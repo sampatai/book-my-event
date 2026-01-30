b0e8fd58ca7aeb0c4e3807a331d845e5dade590d
sampat1 <sampat.adhikari@gmail.com>
2026-01-30 22:24:06 +1000

    added class for crud pandit

---
+++ src/Application/Command/Pandit/UpdatePandit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Command.Pandit
+{
+    public static class UpdatePandit
+    {
+        public sealed record Command() : ICommand<UpdatePanditResponse>;
+
+        internal sealed class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+            }
+        }
+        internal sealed class UpdatePanditHandler() : ICommandHandler<Command, UpdatePanditResponse>
+        {
+            public Task<Result<UpdatePanditResponse>> Handle(Command command, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public sealed record UpdatePanditResponse();
+    }
+}
