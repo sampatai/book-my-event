46865f51df6e10c13ed99f55cceac2f94135a442
sampat1 <sampat.adhikari@gmail.com>
2026-01-31 22:19:36 +1000

    added create

--- src/Application/Command/Pandit/CreatePandit.cs
+++ src/Application/Command/Pandit/CreatePandit.cs
@@ -1,31 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
+using Application.Abstractions.IRepository;
+using Domain.Pandit.Root;
+using FluentValidation;
+using SharedKernel;
 
-
-namespace Application.Command.Pandit
+namespace Application.Command
 {
     public static class CreatePandit
     {
-        public record Command(string name) : ICommand;
+        #region Command
+        public record Command(
+            long UserId,
+            string FullName,
+            string Languages,
+            int ExperienceInYears,
+            AddressCommand Address
+        ) : ICommand;
 
-        internal sealed class Validator : AbstractValidator<Command>
+        public record AddressCommand(
+            string Street,
+            string City,
+            string State,
+            string PostalCode,
+            string Country,
+            string AddressLine1,
+            string? AddressLine2
+        );
+        #endregion
+
+        #region Validator
+        public class Validator : AbstractValidator<Command>
         {
             public Validator()
             {
+                RuleFor(x => x.FullName)
+                    .NotEmpty().WithMessage("Full name is required.");
 
+                RuleFor(x => x.Languages)
+                    .NotEmpty().WithMessage("Languages are required.");
+
+                RuleFor(x => x.ExperienceInYears)
+                    .GreaterThan(0).WithMessage("Experience must be greater than zero.");
             }
         }
-        internal sealed class CreatePanditHandler()
-           : ICommandHandler<Command>
+        #endregion
+
+        #region Handler
+        internal sealed class Handler : ICommandHandler<Command>
         {
+            private readonly IPanditRepository _panditRepository;
 
-            public Task<Result> Handle(Command command, CancellationToken cancellationToken)
+            public Handler(IPanditRepository panditRepository)
             {
-                throw new NotImplementedException();
+                _panditRepository = panditRepository;
+            }
+
+            public async Task<Result> Handle(Command command, CancellationToken cancellationToken)
+            {
+
+                var pandit = new Pandit(
+                    command.UserId,
+                    command.FullName,
+                    command.Languages,
+                    command.ExperienceInYears
+                );
+                pandit.SetAddress(
+                    addressLine1: command.Address.AddressLine1,
+                    addressLine2: command.Address.AddressLine2,
+                    city: command.Address.City,
+                    postcode: command.Address.PostalCode,
+                    state: command.Address.State,
+                    country: command.Address.Country,
+                    street: command.Address.Street,
+                    timezone: TimeZoneInfo.Local.Id
+                );
+                await _panditRepository.AddAsync(pandit, cancellationToken);
+                await _panditRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+                return Result.Success();
             }
         }
+        #endregion
     }
 }
--- src/Application/Command/Pandit/UpdatePandit.cs
+++ src/Application/Command/Pandit/UpdatePandit.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Application.Command.Pandit
+namespace Application.Command
 {
     public static class UpdatePandit
     {
